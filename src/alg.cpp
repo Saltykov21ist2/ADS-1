1895f73ee69b0eb2f86e63da29e9a0a6d980760b
Saltykov21ist2 <99250746+Saltykov21ist2@users.noreply.github.com>
2022-03-17 14:20:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,45 +1,41 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
-
-
 bool checkPrime(uint64_t value) {
-uint64_t ch = 2;
-  for (ch = 2; ch < value; ch++) {
-  if ((value % ch) == 0) {
-  return false;
-  }
+  if (value <=1)
+    return false;
+  for (int mv = 2; mv <= (value/2); mv++) {
+    if ((value % mv) == 0)
+      return false;
   }
   return true;
 }
 
 uint64_t nPrime(uint64_t n) {
- int pr = 0, ct = 1;
-  while (ct != n){
-  while (pr != n) {
-  ct++;
-  if (checkPrime(ct)) {
-  pr++;
+  uint64_t a = 0;
+  uint64_t mv = 0;
+  while (mv != n) {
+    a++;
+    if (checkPrime(a))
+      mv++;
   }
-  }
-  return ct;
+  return a;
 }
 
 uint64_t nextPrime(uint64_t value) {
-value++;
-  while ((checkPrime(value)) != true){
   value++;
-  }
-  return value;
+    while (checkPrime(value) != true) {
+      value++;
+    }
+    return value;
 }
 
+
 uint64_t sumPrime(uint64_t hbound) {
-  int j = 2;
   uint64_t summ = 0;
-  for (j = 2; j < hbound; j++){
-  if (checkPrime(j)) {
-  summ += j;
-  }
-  }
-  return summ;
+    for (uint64_t mv = 2; mv < hbound; mv++) {
+      if (checkPrime(mv) == true)
+        summ += mv;
+    }
+    return summ;
 }
